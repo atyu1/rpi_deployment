14c49b32f001bda61c23d5fbef98969db189e59d
atyu <attila.sovak@gmail.com>
2021-04-11 20:06:01 +0200

    Fix download and decompress + add dd copy

--- create_bootable_raspbian.sh
+++ create_bootable_raspbian.sh
@@ -7,25 +7,36 @@
 # 3. Copy with dd
 ##########################
 
-# 1. Download
+
+RASPBIAN_URL="https://downloads.raspberrypi.org/raspbian_lite_latest"
+CARD_DEV="sdb"
+
+# 1. Download and decompress
 if [ $# -gt 0 ]; then
     if [ $1 = "-d" ]; then
-        curl -JLO https://downloads.raspberrypi.org/raspbian_lite_latest 
+        curl -JLO $RASPBIAN_URL
+
+        filename=$(ls | grep *raspbian*lite*)
+        zipfilename=$filenamei.zip
+
+        mv $filename $zipfilename
+
+        echo $zipfilename
+        unzip $zipfilename
+
     fi
 fi
 
-if [ ! -f ./*raspbian*lite.zip ]; then
+if [ ! -f *raspbian*lite.img ]; then
     echo "ERROR: File *raspban*lite.zip is not in current dir, use -d to download"
     exit 1
 fi
 
-# 2. Decompress it
-filename=$(ls | grep *raspbian*lite.zip)
-echo $filename
-gunzip -S .zip $filename
 
+# 3. Copy with dd
 
-
+imgfilename=$(ls | grep *raspbian*lite*.img)
+sudo dd if=$imgfilename of=/dev/$CARD_DEV bs=4M conv=fsync status=progress
 
 echo "DONE"
 exit 0
