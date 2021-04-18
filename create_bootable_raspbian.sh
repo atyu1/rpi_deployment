b6a442667cabd35f3eae7227c730ad64c2529066
atyu1 <attila.sovak@gmail.com>
2021-04-18 14:02:37 +0200

    Add script

--- create_bootable_raspbian.sh
+++ create_bootable_raspbian.sh
@@ -11,9 +11,16 @@
 RASPBIAN_URL="https://downloads.raspberrypi.org/raspbian_lite_latest"
 CARD_DEV="sdb"
 
+SSID=$1
+PSK=$2
+IMAGE_FILE=""
+
+echo $1 
+echo $2
+
 # 1. Download and decompress
 if [ $# -gt 0 ]; then
-    if [ $1 = "-d" ]; then
+    if [ $3 = "-d" ]; then
         curl -JLO $RASPBIAN_URL
 
         filename=$(ls | grep *raspbian*lite*)
@@ -24,6 +31,35 @@ if [ $# -gt 0 ]; then
         echo $zipfilename
         unzip $zipfilename
 
+		# 2. Add Wifi details to img file
+
+		IMAGE_FILE=$(ls | grep *raspbian*lite*.img)
+        echo $IMAGE_FILE
+		TMP=$(mktemp -d)
+		LOOP=$(sudo losetup --show -fP "${IMAGE_FILE}")
+		sudo mount ${LOOP}p2 $TMP
+		sudo mount ${LOOP}p1 $TMP/boot/
+
+        echo $TMP
+		#create supplicant file
+		cat << EOF >> $TMP/boot/wpa_supplicant.conf
+ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+update_config=1
+country=SK
+
+network={
+ ssid="$SSID"
+ psk="$PSK"
+}
+EOF
+
+		touch $TMP/boot/ssh
+	
+		# cleanup
+		umount ${TMP}/boot/
+		umount ${TMP}
+		rmdir ${TMP}
+
     fi
 fi
 
@@ -32,11 +68,10 @@ if [ ! -f *raspbian*lite.img ]; then
     exit 1
 fi
 
-
 # 3. Copy with dd
+IMAGE_FILE=$(ls | grep *raspbian*lite*.img)
 
-imgfilename=$(ls | grep *raspbian*lite*.img)
-sudo dd if=$imgfilename of=/dev/$CARD_DEV bs=4M conv=fsync status=progress
+sudo dd if=$IMAGE_FILE of=/dev/$CARD_DEV bs=4M conv=fsync status=progress
 
 echo "DONE"
 exit 0
